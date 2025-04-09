0e86a6a5a043a8e386c733512414155a6d5d9e0d
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-09 22:10:50 +0500

    feat: content component added

--- src/app/(layout-header)/layout.tsx
+++ src/app/(layout-header)/layout.tsx
@@ -1,15 +1,18 @@
-import type { ReactNode } from 'react'
+import type { ReactNode } from 'react';
 
-import Header from '@component/common/header/header'
-import { CookiesProvider } from 'next-client-cookies/server'
+import { CookiesProvider } from 'next-client-cookies/server';
+import Header from '@component/common/header/header';
+import Content from '@component/common/content/content';
 
 const HeaderLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
-    return (
-        <CookiesProvider>
-            <Header />
-            <main>{children}</main>
-        </CookiesProvider>
-    )
-}
+  return (
+    <CookiesProvider>
+      <Header />
+      <main>
+        <Content>{children}</Content>
+      </main>
+    </CookiesProvider>
+  );
+};
 
-export default HeaderLayout
+export default HeaderLayout;
