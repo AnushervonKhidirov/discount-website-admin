de7e44eb5d902bb1b09e49369e5eb6b9f01bef09
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-09 22:10:07 +0500

    feat: additional props added

--- src/component/common/header/header.tsx
+++ src/component/common/header/header.tsx
@@ -1,11 +1,16 @@
+import Content from '../content/content';
 import ProfileBtn from '../profile-btn/profile-btn';
+import Navigation from '../navigation/navigation';
 
-import classes from './header.module.css'
+import classes from './header.module.css';
 
 const Header = () => {
   return (
     <header className={classes.header}>
-      <ProfileBtn />
+      <Content className={classes.header_content}>
+        <ProfileBtn className={classes.profile_btn} />
+        <Navigation className={classes.navigation} />
+      </Content>
     </header>
   );
 };
