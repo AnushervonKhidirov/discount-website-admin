08810f59efbfab7f6806645f2b365ff23111e05e
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-09 21:33:45 +0500

    header added

--- src/app/(layout-nav)/layout.tsx
+++ src/app/(layout-nav)/layout.tsx
@@ -1,12 +1,12 @@
 import type { ReactNode } from 'react'
 
-// import Header from '@common/header/header'
+import Header from '@component/common/header/header'
 import { CookiesProvider } from 'next-client-cookies/server'
 
 const HeaderLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
     return (
         <CookiesProvider>
-            {/* <Header /> */}
+            <Header />
             <main>{children}</main>
         </CookiesProvider>
     )
