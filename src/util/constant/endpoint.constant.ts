76bde2bcd46a32feabc9e61cc5562aeca01a422f
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-09 21:00:22 +0500

    feat: new pages and endpoints added

--- src/util/constant/endpoint.constant.ts
+++ src/util/constant/endpoint.constant.ts
@@ -1,5 +1,6 @@
 export enum Endpoint {
   Signin = 'http://localhost:4000/auth/sign-in',
   GetUser = 'http://localhost:4000/users/:id',
+  GetAllUsers = 'http://localhost:4000/users',
   RefreshToken = 'http://localhost:4000/auth/refresh-token',
 }
