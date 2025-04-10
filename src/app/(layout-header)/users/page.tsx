e94bc2d4094a00879b9d35a9855e21e5d9e3a47c
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-10 13:03:39 +0500

    users page updated

--- src/app/(layout-header)/users/page.tsx
+++ src/app/(layout-header)/users/page.tsx
@@ -4,6 +4,7 @@ import type { User } from '@type/user.type';
 import { useEffect, useState } from 'react';
 import { notification, Table } from 'antd/es';
 import { UserService } from '@service/user/user.service';
+import { requestWithRefresh } from '@helper/request.helper';
 
 import { columns } from './coldef';
 
@@ -11,22 +12,15 @@ const UsersPage = () => {
   const [api, context] = notification.useNotification();
   const userService = new UserService();
   const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
 
   async function getUsers() {
-    setIsLoading(true);
-
-    const [users, err] = await userService.getAllUsers();
+    const [users, err] = await requestWithRefresh(() => userService.getAllUsers());
 
     if (err) {
       api.error({ message: err.error, description: err.message });
     } else {
       setUsers(users);
-      console.log(new Date(users[0].createdAt));
     }
-
-    setIsLoading(false);
-    console.log(isLoading);
   }
 
   useEffect(() => {
@@ -34,10 +28,10 @@ const UsersPage = () => {
   }, []);
 
   return (
-    <div>
+    <>
       {context}
       <Table<User> dataSource={users} columns={columns} rowKey={({ id }) => id} />
-    </div>
+    </>
   );
 };
 
