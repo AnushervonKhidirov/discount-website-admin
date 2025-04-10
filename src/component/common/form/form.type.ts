6dc0902dcdc66e1a12978bbff2e3fe679ac42592
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-10 13:02:19 +0500

    feat: form updated

--- src/component/common/form/form.type.ts
+++ src/component/common/form/form.type.ts
@@ -4,4 +4,5 @@ export type FormProps<> = AdditionalProps<{
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onSubmit: (values: any) => Promise<void>;
   title?: string;
+  submitBtnText?: string;
 }>;
