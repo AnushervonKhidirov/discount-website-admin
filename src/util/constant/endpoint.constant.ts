5fbe396f9abf36adff6cfbb6d31103927093a41f
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-14 10:17:58 +0500

    feat: bank endpoints added

--- src/util/constant/endpoint.constant.ts
+++ src/util/constant/endpoint.constant.ts
@@ -1,6 +1,8 @@
 export enum Endpoint {
   Signin = 'http://localhost:4000/auth/sign-in',
+  RefreshToken = 'http://localhost:4000/auth/refresh-token',
   GetUser = 'http://localhost:4000/users/:id',
   GetAllUsers = 'http://localhost:4000/users',
-  RefreshToken = 'http://localhost:4000/auth/refresh-token',
+  GetBank = 'http://localhost:4000/banks/:id',
+  GetAllBanks = 'http://localhost:4000/banks',
 }
